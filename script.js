3e9ad62f2e719b71b89cfe5e1df24dc7288c885f
RafaelSRV <rafael-srv@hotmail.com>
2024-01-09 19:07:24 -0300

    Subindo projeto bugado

--- script.js
+++ script.js
@@ -1,60 +1,91 @@
 var users = []
 
-function getUser(){
-    var Texto = document.getElementById("inputText").value
-    // if(Texto.includes(users[0].name)){
-    //     console.log('ok')
-    // }
-    users[0].name.includes(Texto) ? console.log('ok') : console.log('não') 
-    console.log(Texto)    
-}
+var text = document.getElementById('inputText')
+
+
+async function getUser(){
 
 const url = 'https://jsonplaceholder.typicode.com/users'
-async function Search(){
 await fetch(url).then(response => response.json())
 .then(data => users = [...data])
 
-users.map( data =>{
+function getTextValue(){
+    var textValue = text.value
 
-let newTr = document.createElement("tr")
-let newTd1 = document.createElement("td")
-var newTd2 = document.createElement("td")
-var newTd3 = document.createElement("td")
-let elements = document.getElementById('elements')
+    let filteredUsers = []
+    users.map(data =>{
+        if(data.name.includes(textValue)){
+            filteredUsers.push(data)
+            
+        }
+        
+    })
+    console.log(filteredUsers.length)
+       if (filteredUsers.length == 0){
+        users.forEach(data =>{
 
-newTd1.textContent = data.name
-newTd2.textContent = data.username
-newTd3.textContent = data.email
-newTr.appendChild(newTd1)
-newTr.appendChild(newTd2)
-newTr.appendChild(newTd3)
-elements.appendChild(newTr)
-})
+            let newTr = document.createElement("tr")
+            let newTd1 = document.createElement("td")
+            var newTd2 = document.createElement("td")
+            var newTd3 = document.createElement("td")
+            let elements = document.getElementById('elements')
+            
+            newTd1.textContent = data.name
+            newTd2.textContent = data.username
+            newTd3.textContent = data.email
+            newTr.appendChild(newTd1)
+            newTr.appendChild(newTd2)
+            newTr.appendChild(newTd3)
+            elements.appendChild(newTr)
+            
+            })
+       } else {
+    filteredUsers.forEach(data =>{
 
-
-
-// users.map((data) => {
-//     var createTr = document.createElement('tr')
-//     createTr.setAttribute("id", n++)
-//     var createTd = document.createElement('td')
-    
-//     structure.appendChild(createTr).appendChild(createTd).innerText = data.name
-//     elemento.appendChild(createTd).innerText = data.username
-//     elemento.appendChild(createTd).innerText = data.email
-
-//     var structure = document.getElementById('elements')
-    
-    
-    
-//     //createTd.innerText = data.username
-//     //rowElement.appendChild(createTd)
-    
-//     console.log(data.username)
-// })
+        let newTr = document.createElement("tr")
+        let newTd1 = document.createElement("td")
+        var newTd2 = document.createElement("td")
+        var newTd3 = document.createElement("td")
+        let elements = document.getElementById('elements')
+        
+        newTd1.textContent = data.name
+        newTd2.textContent = data.username
+        newTd3.textContent = data.email
+        newTr.appendChild(newTd1)
+        newTr.appendChild(newTd2)
+        newTr.appendChild(newTd3)
+        elements.appendChild(newTr)
+        
+        })}
+    }
+    getTextValue()
+    text.onchange = getTextValue
 
 }
 
-Search()
+getUser()
+// filteredUsers.forEach(data =>{
+
+//             let newTr = document.createElement("tr")
+//             let newTd1 = document.createElement("td")
+//             var newTd2 = document.createElement("td")
+//             var newTd3 = document.createElement("td")
+//             let elements = document.getElementById('elements')
+            
+//             newTd1.textContent = data.name
+//             newTd2.textContent = data.username
+//             newTd3.textContent = data.email
+//             newTr.appendChild(newTd1)
+//             newTr.appendChild(newTd2)
+//             newTr.appendChild(newTd3)
+//             elements.appendChild(newTr)
+            
+//             })
+    
+
+
+
+
 
 
 
